ca5c83163e315651165f84501c02c6a4d2b36117
VadimSel <selyaaa33@gmail.com>
2024-10-15 18:12:54 +0300

    added Manage Members

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -3,7 +3,7 @@ import { create } from "zustand";
 
 // Файл для управления логикой модалок (закрытие, открытие, передача данных и тд)
 
-export type ModalType = "createServer" | "invite" | "editServer";
+export type ModalType = "createServer" | "invite" | "editServer" | "members";
 
 interface ModalData {
   server?: Server;
