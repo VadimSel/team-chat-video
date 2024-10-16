e6fab80d355af4533c5ded1855a66533699e6843
VadimSel <selyaaa33@gmail.com>
2024-10-16 19:29:18 +0300

    added delete and edit channel functional

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,4 +1,4 @@
-import { ChannelType, Server } from "@prisma/client";
+import { Channel, ChannelType, Server } from "@prisma/client";
 import { create } from "zustand";
 
 // Файл для управления логикой модалок (закрытие, открытие, передача данных и тд)
@@ -10,10 +10,13 @@ export type ModalType =
   | "members"
   | "createChannel"
   | "leaveServer"
-  | "deleteServer";
+  | "deleteServer"
+  | "deleteChannel"
+  | "editChannel";
 
 interface ModalData {
   server?: Server;
+  channel?: Channel
   channelType?: ChannelType
 }
 
