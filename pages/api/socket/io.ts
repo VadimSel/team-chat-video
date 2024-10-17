6dd951cdd278fa19de944759e06364f4b612a274
VadimSel <selyaaa33@gmail.com>
2024-10-17 22:16:47 +0300

    fix for deploy

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -1,32 +1,3 @@
-import { Server as NetServer } from "http";
-import { NextApiRequest } from "next";
-import { Server as ServerIO } from "socket.io";
-
-import { NextApiResponseServerIo } from "@/types";
-
-export const config = {
-  api: {
-    bodyParser: false
-  }
-}
-
-const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
-  if (!res.socket.server.io) {
-    const path = "/api/socket/io"
-    const httpServer: NetServer = res.socket.server as any
-    const io = new ServerIO(httpServer, {
-      path: path,
-      addTrailingSlash: false,
-    })
-    res.socket.server.io = io
-  }
-
-  res.end()
-}
-
-export default ioHandler
-
-
 // import { Server as NetServer } from "http";
 // import { NextApiRequest } from "next";
 // import { Server as ServerIO } from "socket.io";
@@ -37,21 +8,51 @@ export default ioHandler
 //   api: {
 //     bodyParser: false
 //   }
-// };
+// }
 
 // const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
 //   if (!res.socket.server.io) {
-//     const path = "/api/socket/io";
-//     const httpServer: NetServer = res.socket.server as any;
+//     const path = "/api/socket/io"
+//     const httpServer: NetServer = res.socket.server as any
 //     const io = new ServerIO(httpServer, {
-//       path,
+//       path: path,
 //       // @ts-ignore
-//       addTrailingSlash: false
-//     });
-//     res.socket.server.io = io;
+//       addTrailingSlash: false,
+//     })
+//     res.socket.server.io = io
 //   }
 
-//   res.end();
-// };
+//   res.end()
+// }
 
-// export default ioHandler;
+// export default ioHandler
+
+
+import { Server as NetServer } from "http";
+import { NextApiRequest } from "next";
+import { Server as ServerIO } from "socket.io";
+
+import { NextApiResponseServerIo } from "@/types";
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
+  if (!res.socket.server.io) {
+    const path = "/api/socket/io";
+    const httpServer: NetServer = res.socket.server as any;
+    const io = new ServerIO(httpServer, {
+      path,
+      // @ts-ignore
+      addTrailingSlash: false
+    });
+    res.socket.server.io = io;
+  }
+
+  res.end();
+};
+
+export default ioHandler;
